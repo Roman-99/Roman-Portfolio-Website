0bfa12ff55684b9f05e8be1489340aeef4371906
R <darkrevanger13@gmail.com>
2024-01-17 15:28:51 -0600

    first commit

---
+++ my-react-app/src/App.tsx
@@ -0,0 +1,11 @@
+import NavBar from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.css";
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
