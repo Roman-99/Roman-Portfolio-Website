7a8c500913e82755693f3d0207d75cad30f1bcb0
R <darkrevanger13@gmail.com>
2024-01-18 18:14:38 -0600

    setup hash routing

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,9 +1,22 @@
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.css";
+import { Routes, Route } from "react-router-dom";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Home from "./routes/Home";
+import Projects from "./routes/Projects";
+import './App.css';
+
 function App() {
   return (
     <div>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
